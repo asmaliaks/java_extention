8f701ffd3d1ab70b10617ccf99a32563710f7957
asmalouski <asmalaiks@gmail.com>
2015-05-31 15:04:20 +0300

    just commit

---
+++ src/Mike.java
@@ -0,0 +1,8 @@
+/**
+ * Created by asmalouski on 31.5.15.
+ */
+public class Mike extends Boy{
+    public String myName(){
+        return "MIke";
+    }
+}
---
+++ src/Peter.java
@@ -0,0 +1,8 @@
+/**
+ * Created by asmalouski on 31.5.15.
+ */
+public class Peter extends Boy{
+    public String myName(){
+        return "Peter";
+    }
+}
